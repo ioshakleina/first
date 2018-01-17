9a21d5a5cc3f8680ce710ae1327e1d985e155a07
ioshakleina <ioshakleina@gmail.com>
2018-01-18 00:16:10 +0200

    Commit17

--- app/src/main/java/info/fandriod/first/MainActivity.java
+++ app/src/main/java/info/fandriod/first/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 //import android.view.Menu;
 //import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,7 +22,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
     Button btnS, btnAdd, btnDiv, btnMul, btn0, btnEq, btnClnr ;
 
+    int oper1, oper2, flagAction;
 
+    double result;
 
 
     @Override
@@ -69,17 +72,115 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn8.setOnClickListener(this);
         btn9.setOnClickListener(this);
 
+        oper1 = 0;
+
+        oper2 = 0;
+
+        result = 0;
+
+        flagAction = 0;
+        LCD.setText(Integer.toString(oper1));
     }
 
     @Override
     public void onClick(View v) {
 
-        LCD.setText("кнопка " + v.getId());
+        switch(v.getId()){
+            case R.id.btn1:
+                ClickNum(1);
+                break;
+            case R.id.btn2:
+                ClickNum(2);
+                break;
+            case R.id.btn3:
+                ClickNum(3);
+                break;
+            case R.id.btn4:
+                ClickNum(4);
+                break;
+            case R.id.btn5:
+                ClickNum(5);
+                break;
+            case R.id.btn6:
+                ClickNum(6);
+                break;
+            case R.id.btn7:
+                ClickNum(7);
+                break;
+            case R.id.btn8:
+                ClickNum(8);
+                break;
+            case R.id.btn9:
+                ClickNum(9);
+                break;
+            case R.id.btn0:
+                ClickNum(0);
+                break;
+            case R.id.btnAdd:
+                if(flagAction == 0) flagAction = 1;
+                break;
+            case R.id.btnS:
+                if(flagAction == 0) flagAction = 2;
+                break;
+            case R.id.btnMul:
+                if(flagAction == 0) flagAction = 3;
+                break;
+            case R.id.btnDiv:
+                if(flagAction == 0) flagAction = 4;
+                break;
+            case R.id.btnEq:
+                switch(flagAction){
+                    case 1:
+                        result = oper1 + oper2;
+                        break;
+                    case 2:
+                        result = oper1 - oper2;
+                        break;
+                    case 3:
+                        result = oper1 * oper2;
+                        break;
+                    case 4:
+                        result =(double) oper1 / (double) oper2;
+                        break;
+                    default:
+                        Toast.makeText(this, "Операция не задана", Toast.LENGTH_LONG);
+
+                }
+                if(flagAction != 0){
+                   LCD.setText(Double.toString(result));
+                    oper1 = 0;
+                    oper2 = 0;
+                    result = 0;
+                    flagAction = 0;
+                }
+
+                break;
+
+            case R.id.btnClnr:
+                oper1 = 0;
+                oper2 = 0;
+                result = 0;
+                flagAction = 0;
+               LCD.setText(Integer.toString(oper1));
+                break;
+
+        }
 
 
 
     }
-
+     private void ClickNum(int num) {
+         if (flagAction == 0)
+         {
+             oper1 = oper1 * 10 + num;
+             LCD.setText(Integer.toString(oper1));
+         }
+         else
+         {
+             oper2 = oper2 * 10 + num;
+             LCD.setText(Integer.toString(oper2));
+         }
+     }
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
